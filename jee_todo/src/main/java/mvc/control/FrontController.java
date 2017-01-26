67c4c6fb1f2b20dd68f161467e39e1c3664d5a09
VOB <vobc@live.com>
2017-01-26 14:57:34 +0100

    stuff started on w21

--- jee_todo/src/main/java/mvc/control/FrontController.java
+++ jee_todo/src/main/java/mvc/control/FrontController.java
@@ -3,10 +3,17 @@
  */
 package mvc.control;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mvc.model.Model;
+import mvc.model.User;
+
 
 /**
  *
@@ -19,6 +26,43 @@ public class FrontController extends HttpServlet {
     private static final Level I = Level.INFO;
     
     // TODO A lot. Handle all incomming POST's
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String action = req.getParameter("action");
+        LOG.log(I, "Action {0}", action);
+        String name;
+        String email;
+        String pwd;
+                
+        switch (action) {
+            case "add":
+                break;
+            case "delete":
+                break;
+            case "login":
+                email = req.getParameter("email");
+                pwd = req.getParameter("password");
+                User ussr = Model.INSTANCE.login(email, pwd);
+                
+                if (ussr == null) {
+                    resp.sendRedirect("/todo/todo.jsp");
+                }
+                break;
+            case "register":
+                name = req.getParameter("name");
+                email = req.getParameter("email");
+                pwd = req.getParameter("password");
+                
+                Model.INSTANCE.register(name, pwd, email); //TODO
+                resp.sendRedirect("/todo/list.jsp");
+                break;
+            default: ;
+        }
+         // PRG pattern
+        
+    }
     
     
     
--- jee_todo/src/main/java/mvc/model/Model.java
+++ jee_todo/src/main/java/mvc/model/Model.java
@@ -10,7 +10,7 @@ public enum Model {
 
     private final TodoList list = new TodoList();
     private final UserRegistry reg = new UserRegistry();
-
+    
     public TodoList getList() {
         return list;
     }
@@ -21,16 +21,25 @@ public enum Model {
 
     
     public User login(String email, String passwd) {
+        User tmp = new User("tmp", passwd, email);
+        User real = reg.lookup(tmp);
         
-        // TODO
+        if (real != null && passwd.equals(real.getPassword())) {
+            return real;
+        }
        
         return null;
     }
 
     public User register(String name, String password, String email) {
         
-        // TODO
-       
+        User newUser = new User(name, password, email);
+        
+        if (reg.lookup(newUser) == null) {
+            reg.register(newUser);
+            return newUser;
+        }
+        
         return null;
     }
 
