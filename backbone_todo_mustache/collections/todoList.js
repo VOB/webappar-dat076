2657df039778dffdf5ca42266e0dc1be8e95ef32
VOB <vobc@live.com>
2017-03-07 14:15:55 +0100

    added node_todo backend

--- backbone_todo_mustache/collections/todoList.js
+++ backbone_todo_mustache/collections/todoList.js
@@ -29,20 +29,21 @@ define([
         // Using browsers localstore
         // MUST: Have this else Backbone will try to access server
         // i.e. must use and URL when saving notes
-        localStorage: new Store('todos-backbone'),
+        // localStorage: new Store('todos-backbone'),
 
+        url: 'http://localhost:4848/todo/list',
         // This is the server side id (cid created clientside automatically by Backbone)
         modelId: function(attributes) {
             return attributes.id;
         },
 
-        initialize: function() {
-            // This will end up in browser local storage
-            // Use dev tools > resources to inspect
-            this.create(1, 'Äta gröt');
-            this.create(2, 'Rensa sallad');
-            this.create(3, 'Jaga älg');
-        }
+        // initialize: function() {
+        //     // This will end up in browser local storage
+        //     // Use dev tools > resources to inspect
+        //     this.create(1, 'Äta gröt');
+        //     this.create(2, 'Rensa sallad');
+        //     this.create(3, 'Jaga älg');
+        // }
 
     });
     // Return the constructor function (the "class"), see app.js for use
--- backbone_todo_mustache/models/todoNote.js
+++ backbone_todo_mustache/models/todoNote.js
@@ -11,35 +11,37 @@ define([
     'use strict';
 
     // (Private) Object for generating serial numbers
-    var counter = {
-        value: 100,
-        getNext: function() {
-            var i = this.value;
-            this.value++;
-            return i;
-        }
-    }
-    /*
-          Many functions defined by Backbone (i.e. there are hidden functions)
-          - TodoNote.destroy()
-          - TodoList.set( ...)
-          - etc. see Backbone docs.
-        */
+    // var counter = {
+    //     value: 100,
+    //     getNext: function() {
+    //         var i = this.value;
+    //         this.value++;
+    //         return i;
+    //     }
+    // }
+    // /*
+        //   Many functions defined by Backbone (i.e. there are hidden functions)
+        //   - TodoNote.destroy()
+        //   - TodoList.set( ...)
+        //   - etc. see Backbone docs.
+        // */
     // Constructor function
     var TodoNote = Backbone.Model.extend({
-        defaults: {
-            text: "empty",
-            done: false
-        },
+        // defaults: {
+        //     text: "empty",
+        //     done: false
+        // },
+        urlRoot: 'http://localhost:4848/todo/list',
 
-        initialize: function(id, text) {
-            // Will set attributes, see model.attributes
-            this.set("id", (!id)
-                ? counter.getNext()
-                : id);
-            this.set("text", text);
-            this.set("dateTime", new Date()); // Must have
-        },
+        idAttribute: "id"
+        // initialize: function(id, text) {
+        //     // Will set attributes, see model.attributes
+        //     this.set("id", (!id)
+        //         ? counter.getNext()
+        //         : id);
+        //     this.set("text", text);
+        //     this.set("dateTime", new Date()); // Must have
+        // },
         //url: function() {}
 
     });
